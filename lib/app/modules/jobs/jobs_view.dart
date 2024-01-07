db85f8183833f12cbfbc4ec613664d02d7952038
MAjd <Zarqanmajd@gmail.com>
2024-01-07 06:20:24 +0300

    new app

---
+++ lib/app/modules/jobs/jobs_view.dart
@@ -0,0 +1,15 @@
+// import 'package:flutter/material.dart';
+
+// class extends StatefulWidget {
+//   const jO({super.key});
+
+//   @override
+//   State<jO> createState() => _jOState();
+// }
+
+// class _jOState extends State<jO> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Placeholder();
+//   }
+// }
